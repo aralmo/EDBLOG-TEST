dfc2fe8283eacc38270fb49e576d58a908352664
Aridane Alamo <aridane.alamo@codurance.com>
2023-02-26 10:33:30 +0100

    Add Post controller integration test. Will improve test coverage webapi startup.

--- src/webapi.tests/PostControllerShould.cs
+++ src/webapi.tests/PostControllerShould.cs
@@ -1,22 +1,80 @@
 
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 using EDBlog.WebAPI.Controllers;
 using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc.Testing;
 
+[Trait("type", "integration")]
 public class PostControllerShould
+    : IClassFixture<WebApplicationFactory<EDBlog.WebAPI.Program>>
 {
-    PostController Controller = new PostController();
+    private readonly WebApplicationFactory<EDBlog.WebAPI.Program> clientFactory;
+    public PostControllerShould(WebApplicationFactory<EDBlog.WebAPI.Program> clientFactory)
+    {
+        this.clientFactory = clientFactory;
+    }
 
     [Theory]
-    [InlineData("some-random-text-nonparseble-to-guid")]
     [InlineData("")]
     [InlineData(null)]
-    public void BadRequest_IfAuthorInvalid(string authorId) 
-        => Controller
-            .POST(new PostController.NewPostRequest(authorId,ValidPost))
-            .Should()
-            .BeOfType<BadRequest>();
+    [InlineData("some-random-text-nonparseble-to-guid")]
+    public async void Return_BadRequest_IfAuthorInvalid(string authorId)
+    {
+        var client = clientFactory.CreateClient();
 
-    static PostController.Post ValidPost 
-        => new ("post title", "post description", "post contents");
+        var result = await client
+            .PostAsync("/post", JsonFor(
+                new 
+                {
+                    AuthorId = authorId
+                }));
+
+        var content = await result.Content.ReadAsStringAsync();
+
+
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        content.Should().Contain(".AuthorId"); //
+    }
+
+    [Fact]
+    public async void Return_BadRequest_IfNoModelSent()
+    {
+        var client = clientFactory.CreateClient();
+
+        var result = await client
+            .PostAsync("/post",new StringContent("", Encoding.UTF8, "application/json"));
+
+        var content = await result.Content.ReadAsStringAsync();
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+    }
+
+    [Theory]
+    [InlineData("less/10")]
+    [InlineData(null)]
+    public async void Return_BadRequest_IfTitleInvalid(string title)
+    {
+        var client = clientFactory.CreateClient();
+
+        var result = await client
+            .PostAsync("/post", JsonFor(
+                new 
+                {
+                    AuthorId = Guid.NewGuid().ToString(), 
+                    Post = new {
+                        Title = title
+                    }
+                }));
+
+        var content = await result.Content.ReadAsStringAsync();
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        content.Should().Contain(".Title"); //
+    }
+
+    static HttpContent JsonFor(object request)
+        => new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
 }
