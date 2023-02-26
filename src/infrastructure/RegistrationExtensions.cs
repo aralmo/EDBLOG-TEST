2cc7c5c841df3d74ec1601014824b29f4d0d44ea
Aridane Alamo <aridane.alamo@codurance.com>
2023-02-26 14:15:10 +0100

    add publisher registration extension. pending masstransit setup.

--- src/infrastructure/RegistrationExtensions.cs
+++ src/infrastructure/RegistrationExtensions.cs
@@ -1,6 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+
 namespace EDBlog.Infrastructure;
 
 public static class RegistrationExtensions
 {
-
+    public static IServiceCollection SetupPublisher(this IServiceCollection services)
+    {
+        //ToDo: register mass transit and publisher.
+        services.AddSingleton<IMediator>(new MassTransitMediator(null));
+        return services;
+    }
 }
