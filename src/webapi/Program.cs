e6e1e3043d9904049bb6fdace5f99028b3a44311
Aridane Alamo <aridane.alamo@codurance.com>
2023-02-26 01:24:17 +0100

    add fluent validations to webapi

--- src/webapi/Program.cs
+++ src/webapi/Program.cs
@@ -1,11 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
+using Microsoft.AspNetCore.Mvc;
 
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    //using fluent validation instead.
+    options.SuppressModelStateInvalidFilter = true;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
