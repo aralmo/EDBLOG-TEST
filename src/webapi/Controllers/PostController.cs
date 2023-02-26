519e1f0d85b0608287c8294ea3c8206bf298f789
Aridane Alamo <aridane.alamo@codurance.com>
2023-02-26 01:24:17 +0100

    Revert "add fluent validations to webapi"
    This reverts commit e6e1e3043d9904049bb6fdace5f99028b3a44311.
    
    Gonna skip unit tests for this project, moving back to modelvalidation
    
    # Conflicts:
    #	src/webapi/Program.cs

--- src/webapi/Controllers/PostController.cs
+++ src/webapi/Controllers/PostController.cs
@@ -1,16 +1,15 @@
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EDBlog.WebAPI.Controllers;
 
 [Route("post")]
-public class PostController:Controller
+public class PostController : Controller
 {
     [HttpPost]
-    public IActionResult POST(NewPostRequest request){
-        throw new NotImplementedException();
-    }
-
+    public IResult POST(NewPostRequest request)
+        => throw new NotImplementedException();
+        
     public record NewPostRequest(string AuthorId, Post post);
     public record Post(string Title, string Description, string Content);
-
 }
