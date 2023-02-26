9ba7c39a21e8e6bc13dd00e3e7061a938700757b
Aridane Alamo <aridane.alamo@codurance.com>
2023-02-26 01:52:52 +0100

    move to classic style program to allow integration test

--- src/webapi/Program.cs
+++ src/webapi/Program.cs
@@ -1,26 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+namespace EDBlog.WebAPI;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.Configure<ApiBehaviorOptions>(options =>
+public class Program
 {
-    //using fluent validation instead.
-    options.SuppressModelStateInvalidFilter = true;
-});
-var app = builder.Build();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Add services to the container.
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.Configure<ApiBehaviorOptions>(options =>
+        {
+            //using fluent validation instead.
+            options.SuppressModelStateInvalidFilter = true;
+        });
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.MapControllers();
+        app.Run();
+    }
 }
-
-app.MapControllers();
-app.Run();
