5ac34a7e5a5b9830cd0d9ec194671f0fc36022dd
Aridane Alamo <aridane.alamo@codurance.com>
2023-02-27 23:41:06 +0100

    implement getpost request consumer

--- src/integration-tests/Service/CreatePostCommandConsumerShould.cs
+++ src/integration-tests/Service/CreatePostCommandConsumerShould.cs
@@ -6,6 +6,7 @@ using EventStore.Client;
 using FluentAssertions;
 using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace Integration.Service;
 
@@ -19,7 +20,7 @@ public class CreatePostCommandConsumerShould
     public CreatePostCommandConsumerShould()
     {
         var services = new ServiceCollection();
-            //run docker compose up in this project root to start dependencies.
+        //run docker compose up in this project root to start dependencies.
         services.AddEventStoreClient("esdb://admin:changeit@localhost:2213?tls=false");
         services.AddLogging();
         services.AddTransient<CreatePostCommandConsumer>();
@@ -41,7 +42,10 @@ public class CreatePostCommandConsumerShould
         };
 
         //Act
-        await consumer.Consume(new FakeConsumeContext<CreatePostCommandContract>(contract));
+        var mock =new Mock<ConsumeContext<CreatePostCommandContract>>();
+        mock.Setup(m => m.Message).Returns(contract);
+
+        await consumer.Consume(mock.Object);
 
         //Assert
         var authorReader = esClient.ReadStreamAsync(
@@ -49,24 +53,24 @@ public class CreatePostCommandConsumerShould
             $"authorposts-{contract.AuthorId}",
             StreamPosition.Start);
 
-            // the author stream tracks author posts
+        // the author stream tracks author posts
         var authorStreamEvent = await authorReader.FirstAsync();
         authorStreamEvent.Should().NotBeNull();
         authorStreamEvent.Event?.EventType?.Should().Be("NewPost");
 
-            // check the contents
+        // check the contents
         Encoding.UTF8
             .GetString(authorStreamEvent.Event!.Data.ToArray())
             .Should()
             .Be(JsonSerializer.Serialize(new
-                {
-                    contract.PostId,
-                    contract.Title,
-                    contract.Description
-                }));
+            {
+                contract.PostId,
+                contract.Title,
+                contract.Description
+            }));
     }
 
-     [Fact]
+    [Fact]
     public async void CreateEvent_InPostStream()
     {
         //Arrange
@@ -81,8 +85,10 @@ public class CreatePostCommandConsumerShould
         };
 
         //Act
-        await consumer.Consume(new FakeConsumeContext<CreatePostCommandContract>(contract));
+        var mock =new Mock<ConsumeContext<CreatePostCommandContract>>();
+        mock.Setup(m => m.Message).Returns(contract);
 
+        await consumer.Consume(mock.Object);
         //Assert
         var postReader = esClient.ReadStreamAsync(
             Direction.Forwards,
@@ -92,18 +98,18 @@ public class CreatePostCommandConsumerShould
         var postStreamEvent = await postReader.FirstAsync();
         postStreamEvent.Should().NotBeNull();
         postStreamEvent.Event?.EventType?.Should().Be("NewPost");
-        
-            // check the contents
+
+        // check the contents
         Encoding.UTF8
             .GetString(postStreamEvent.Event!.Data.ToArray())
             .Should()
             .Be(JsonSerializer.Serialize(new
-                {
-                    contract.AuthorId,
-                    contract.Title,
-                    contract.Description,
-                    contract.Content
-                }));
+            {
+                contract.AuthorId,
+                contract.Title,
+                contract.Description,
+                contract.Content
+            }));
 
     }
 
@@ -118,223 +124,5 @@ public class CreatePostCommandConsumerShould
         public string? Description { get; set; }
 
         public string? Content { get; set; }
-    }
-    class FakeConsumeContext<T> : ConsumeContext<T> where T : class
-    {
-        T message;
-        public T Message => message;
-
-        public FakeConsumeContext(T message) => this.message = message;
-
-        public ReceiveContext ReceiveContext => throw new NotImplementedException();
-
-        public SerializerContext SerializerContext => throw new NotImplementedException();
-
-        public Task ConsumeCompleted => throw new NotImplementedException();
-
-        public IEnumerable<string> SupportedMessageTypes => throw new NotImplementedException();
-
-        public CancellationToken CancellationToken => throw new NotImplementedException();
-
-        public Guid? MessageId => throw new NotImplementedException();
-
-        public Guid? RequestId => throw new NotImplementedException();
-
-        public Guid? CorrelationId => throw new NotImplementedException();
-
-        public Guid? ConversationId => throw new NotImplementedException();
-
-        public Guid? InitiatorId => throw new NotImplementedException();
-
-        public DateTime? ExpirationTime => throw new NotImplementedException();
-
-        public Uri? SourceAddress => throw new NotImplementedException();
-
-        public Uri? DestinationAddress => throw new NotImplementedException();
-
-        public Uri? ResponseAddress => throw new NotImplementedException();
-
-        public Uri? FaultAddress => throw new NotImplementedException();
-
-        public DateTime? SentTime => throw new NotImplementedException();
-
-        public Headers Headers => throw new NotImplementedException();
-
-        public HostInfo Host => throw new NotImplementedException();
-
-        public void AddConsumeTask(Task task)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T1 AddOrUpdatePayload<T1>(PayloadFactory<T1> addFactory, UpdatePayloadFactory<T1> updateFactory) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public ConnectHandle ConnectPublishObserver(IPublishObserver observer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ConnectHandle ConnectSendObserver(ISendObserver observer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T1 GetOrAddPayload<T1>(PayloadFactory<T1> payloadFactory) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ISendEndpoint> GetSendEndpoint(Uri address)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool HasMessageType(Type messageType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool HasPayloadType(Type payloadType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task NotifyConsumed(TimeSpan duration, string consumerType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task NotifyConsumed<T1>(ConsumeContext<T1> context, TimeSpan duration, string consumerType) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task NotifyFaulted(TimeSpan duration, string consumerType, Exception exception)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task NotifyFaulted<T1>(ConsumeContext<T1> context, TimeSpan duration, string consumerType, Exception exception) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Publish<T1>(T1 message, CancellationToken cancellationToken = default) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Publish<T1>(T1 message, IPipe<PublishContext<T1>> publishPipe, CancellationToken cancellationToken = default) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Publish<T1>(T1 message, IPipe<PublishContext> publishPipe, CancellationToken cancellationToken = default) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Publish(object message, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Publish(object message, IPipe<PublishContext> publishPipe, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Publish(object message, Type messageType, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Publish(object message, Type messageType, IPipe<PublishContext> publishPipe, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Publish<T1>(object values, CancellationToken cancellationToken = default) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Publish<T1>(object values, IPipe<PublishContext<T1>> publishPipe, CancellationToken cancellationToken = default) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Publish<T1>(object values, IPipe<PublishContext> publishPipe, CancellationToken cancellationToken = default) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Respond<T1>(T1 message) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RespondAsync<T1>(T1 message) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RespondAsync<T1>(T1 message, IPipe<SendContext<T1>> sendPipe) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RespondAsync<T1>(T1 message, IPipe<SendContext> sendPipe) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RespondAsync(object message)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RespondAsync(object message, Type messageType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RespondAsync(object message, IPipe<SendContext> sendPipe)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RespondAsync(object message, Type messageType, IPipe<SendContext> sendPipe)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RespondAsync<T1>(object values) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RespondAsync<T1>(object values, IPipe<SendContext<T1>> sendPipe) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RespondAsync<T1>(object values, IPipe<SendContext> sendPipe) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool TryGetMessage<T1>(out ConsumeContext<T1>? consumeContext) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool TryGetPayload<T1>(out T1 payload) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
     }
 }
--- src/integration-tests/Service/GetPostRequestConsumerShould.cs
+++ src/integration-tests/Service/GetPostRequestConsumerShould.cs
@@ -6,6 +6,7 @@ using EventStore.Client;
 using FluentAssertions;
 using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace IntegrationTests.Service;
 
@@ -25,4 +26,45 @@ public class GetPostRequestConsumerShould
         serviceProvider = services.BuildServiceProvider();
     }
 
+    [Fact]
+    public async void ReturnPost_IfExists()
+    {
+        //arrange
+        var esClient = serviceProvider.GetRequiredService<EventStoreClient>();
+        Guid postid = Guid.NewGuid();
+        
+        var stub = new 
+                {
+                    AuthorId = Guid.NewGuid(),
+                    Title = "some title",
+                    Description = "post description",
+                    Content = "post contents"
+                };
+
+        await esClient.AppendToStreamAsync($"post-{postid}", StreamState.Any, new[]
+        {            
+            new EventData(Uuid.NewUuid(),"NewPost",
+                JsonSerializer.SerializeToUtf8Bytes(stub))
+        });
+        
+        var mock = new Mock<ConsumeContext<GetPostRequestContract>>();
+        
+        mock.Setup(m => m.Message).Returns(new contract(){
+            PostId = postid
+        });
+
+        //act
+        await consumer.Consume(mock.Object);
+
+        //todo: check content
+        
+        mock.Verify(m => m.RespondAsync<GetPostResponseContract>(It.IsAny<GetPostResponseContract>()),Times.Once);
+        GetPostResponseContract response =(GetPostResponseContract) mock.Invocations.Last().Arguments[0];
+        response.Post.Should().BeEquivalentTo(stub);
+    }
+
+    record contract : GetPostRequestContract
+    {
+        public Guid PostId {get;set;}
+    }
 }
