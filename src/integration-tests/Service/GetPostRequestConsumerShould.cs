171498a6b6c2ee71008197d8ed5d2e59ae685dc8
Aridane Alamo <aridane.alamo@codurance.com>
2023-03-03 00:20:13 +0100

    move requests to request client instead of mediator

--- src/integration-tests/Service/GetPostRequestConsumerShould.cs
+++ src/integration-tests/Service/GetPostRequestConsumerShould.cs
@@ -8,7 +8,7 @@ using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 
-namespace IntegrationTests.Service;
+namespace Integration.Service;
 
 public class GetPostRequestConsumerShould
 {
